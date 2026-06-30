e039e73e2ac2ad1125c8161ce631ad3448f6c6cc
Piotr H <piotr1hyriak@gmail.com>
2026-06-30 14:07:21 +0200

    Add jmx

--- postgres/init/002-notification.sql
+++ postgres/init/002-notification.sql
@@ -1,5 +1,10 @@
 CREATE USER notification_user
 WITH PASSWORD 'uat-notification-password';
 
+GRANT CONNECT ON DATABASE ecommerce TO notification_user;
+
 CREATE SCHEMA notification_service_uat
 AUTHORIZATION notification_user;
+
+GRANT USAGE ON SCHEMA notification_service_uat TO notification_user;
+GRANT CREATE ON SCHEMA notification_service_uat TO notification_user;
--- postgres/init/003-order.sql
+++ postgres/init/003-order.sql
@@ -1,5 +1,10 @@
 CREATE USER order_user
 WITH PASSWORD 'uat-order-password';
 
+GRANT CONNECT ON DATABASE ecommerce TO order_user;
+
 CREATE SCHEMA order_service_uat
 AUTHORIZATION order_user;
+
+GRANT USAGE ON SCHEMA order_service_uat TO order_user;
+GRANT CREATE ON SCHEMA order_service_uat TO order_user;
